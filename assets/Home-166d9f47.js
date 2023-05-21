c6f153857bc9823e762820fe67af6976aa765980
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2023-05-21 17:01:15 +0300

    Adding dist

---
+++ assets/Home-166d9f47.js
@@ -0,0 +1,30 @@
+import{s as e,L as o,j as t}from"./index-3d36b5b4.js";const n=e.h1`
+  text-align: center;
+  padding-top: 8px;
+  margin-bottom: 32px;
+`,r=e(o)`
+  display: block;
+  font-size: 18px;
+  line-height: 1.222;
+  font-weight: 600;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  text-decoration: none;
+  width: 196px;
+
+  padding: 14px 0;
+
+  color: #373737;
+  background: #ebd8ff;
+  box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
+  border-radius: 10.3108px;
+
+  text-transform: uppercase;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  &:hover {
+    background: #5cd3a8;
+  }
+`,i=()=>t.jsxs("section",{children:[t.jsx(n,{children:"Tweets - expands the space of communication"}),t.jsx(r,{to:"/tweets",children:"Start"})]});export{i as default};
---
+++ assets/NotFound-71a5acf3.js
@@ -0,0 +1,5 @@
+import{s as t,j as o}from"./index-3d36b5b4.js";const e=t.div`
+  display: block;
+  text-align: center;
+  padding-top: calc(25vh);
+`,n=()=>o.jsxs(e,{children:[o.jsx("h2",{children:"Not found this page"}),o.jsx("p",{children:" Come back to Home page"})]});export{n as default};
