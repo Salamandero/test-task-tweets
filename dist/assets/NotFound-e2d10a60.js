39369a37a8165e9009382c84020c744f11faf34d
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2023-05-21 16:52:11 +0300

    fix code to github work

--- dist/assets/NotFound-e2d10a60.js
+++
@@ -1,5 +0,0 @@
-import{s as t,j as o}from"./index-cd3945df.js";const e=t.div`
-  display: block;
-  text-align: center;
-  padding-top: calc(25vh);
-`,n=()=>o.jsxs(e,{children:[o.jsx("h2",{children:"Not found this page"}),o.jsx("p",{children:" Come back to Home page"})]});export{n as default};
